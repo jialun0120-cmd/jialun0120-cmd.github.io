35243dea59b8ad5aab6fd5f6222b05d25cbe3772
jialun0120-cmd <jialun0120@gmail.com>
2026-07-09 18:50:36 +0800

    fix startQuiz args

--- quiz-resume.js
+++ quiz-resume.js
@@ -34,7 +34,7 @@ setTimeout(function(){
   try{
     if(typeof _sk===typeof _sk){var o1=_sk;_sk=function(i){o1(i);setTimeout(sv,0)}}
     if(typeof _sm===typeof _sk){var o2=_sm;_sm=function(){o2();setTimeout(sv,0)}}
-    if(typeof startQuiz===typeof _sk){var o3=startQuiz;startQuiz=function(l,s){o3(l,s);clr()}}
+    if(typeof startQuiz===typeof _sk){var o3=startQuiz;startQuiz=function(l,s,ss){o3(l,s,ss);clr()}}
   }catch(e){}
   showResume();
 },200);
